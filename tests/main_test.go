29f437b1130ee88d3a6bf4f64e5faec38644d345
Nano Taboada <87288+nanotaboada@users.noreply.github.com>
2024-04-09 23:09:57 -0300

    feat(tests)!: add test cases for POST, PUT and DELETE

--- tests/main_test.go
+++ tests/main_test.go
@@ -2,9 +2,10 @@
  * Tests
  * -------------------------------------------------------------------------- */
 
-package main
+package tests
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -21,20 +22,23 @@ import (
 func TestMain(main *testing.M) {
 
 	gin.SetMode(gin.TestMode)
-	path := "../data/players-sqlite3.db"
-	data.Connect(path)
+	data.Connect("../data/players-sqlite3.db")
 	os.Exit(main.Run())
 }
 
-const path = "/players/"
+const (
+	Url             = "/players/"
+	ContentType     = "Content-Type"
+	ApplicationJSON = "application/json"
+)
 
 // Given GET, when request to /players (no trailing slash), then response status should be 301 (Moved Permanently)
-func TestGetPlayersNoTrailingSlashStatusMovedPermanently(test *testing.T) {
+func TestRequestGetNoTrailingSlashResponseStatusMovedPermanently(test *testing.T) {
 
 	// Arrange
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", "/players", nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/players", nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
@@ -44,12 +48,12 @@ func TestGetPlayersNoTrailingSlashStatusMovedPermanently(test *testing.T) {
 }
 
 // Given GET, when request has no parameters, then response status should be 200 (OK).
-func TestRequestGetPlayersResponseStatusOK(test *testing.T) {
+func TestRequestGetNoParamResponseStatusOK(test *testing.T) {
 
 	// Arrange
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", path, nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, Url, nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
@@ -58,13 +62,13 @@ func TestRequestGetPlayersResponseStatusOK(test *testing.T) {
 	assert.Equal(test, http.StatusOK, recorder.Code)
 }
 
-// Given GET, when request has no parameters, then response body should be the collection of players.
-func TestRequestGetPlayersResponsePlayers(test *testing.T) {
+// Given GET, when request has no parameters, then response body should be the collection of Players.
+func TestRequestGetNoParamResponsePlayers(test *testing.T) {
 
 	// Arrange
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", path, nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, Url, nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
@@ -75,14 +79,14 @@ func TestRequestGetPlayersResponsePlayers(test *testing.T) {
 	assert.NotEmpty(test, players)
 }
 
-// Given GET, when request parameter identifies existing player, then response status should be 200 (OK).
-func TestRequestGetPlayersIdResponseStatusOK(test *testing.T) {
+// Given GET, when request parameter identifies existing Player, then response status should be 200 (OK).
+func TestRequestGETIdExistingPlayerResponseStatusOK(test *testing.T) {
 
 	// Arrange
 	id := "10"
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", path+id, nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, Url+id, nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
@@ -91,14 +95,14 @@ func TestRequestGetPlayersIdResponseStatusOK(test *testing.T) {
 	assert.Equal(test, http.StatusOK, recorder.Code)
 }
 
-// Given GET, when request parameter identifies existing player, then response body should be matching Player.
-func TestRequestGetPlayersIdResponsePlayer(test *testing.T) {
+// Given GET, when request parameter identifies existing Player, then response body should be matching Player.
+func TestRequestGETIdExistingPlayerResponsePlayer(test *testing.T) {
 
 	// Arrange
 	id := "10"
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", path+id, nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, Url+id, nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
@@ -112,18 +116,162 @@ func TestRequestGetPlayersIdResponsePlayer(test *testing.T) {
 	assert.Equal(test, "Messi", player.LastName)
 }
 
-// Given GET, when request parameter does not identify a player, then response status should be 404 (Not Found).
-func TestRequestGetPlayersIdResponseStatusNotFound(test *testing.T) {
+// Given GET, when request parameter does not identify a Player, then response status should be 404 (Not Found).
+func TestRequestGETIdNonExistingPlayerResponseStatusNotFound(test *testing.T) {
 
 	// Arrange
-	id := "99"
+	id := "999"
 	router := routes.Setup()
-	request, _ := http.NewRequest("GET", path+id, nil)
 	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, Url+id, nil)
 
 	// Act
 	router.ServeHTTP(recorder, request)
 
 	// Assert
 	assert.Equal(test, http.StatusNotFound, recorder.Code)
+}
+
+// Given POST, when request body is empty, then response status should be 400 (Bad Request)
+func TestRequestPOSTBodyEmptyResponseStatusBadRequest(test *testing.T) {
+
+	// Arrange
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPost, Url, nil)
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusBadRequest, recorder.Code)
+}
+
+// Given POST, when request is existing Player, then response status should be 409 (Conflict)
+func TestRequestPOSTBodyExistingPlayerResponseStatusConflict(test *testing.T) {
+
+	// Arrange
+	player := GetExistingPlayer()
+	body, _ := json.Marshal(player)
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPost, Url, bytes.NewBuffer(body))
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusConflict, recorder.Code)
+}
+
+// Given POST, when request is non-existing Player, then response status should be 201 (Created)
+func TestRequestPOSTBodyNonExistingPlayerResponseStatusCreated(test *testing.T) {
+
+	// Arrange
+	player := GetNonExistingPlayer()
+	body, _ := json.Marshal(player)
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPost, Url, bytes.NewBuffer(body))
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusCreated, recorder.Code)
+}
+
+// Given PUT, when request body is empty, then response status should be 400 (Bad Request)
+func TestRequestPUTBodyEmptyResponseStatusBadRequest(test *testing.T) {
+
+	// Arrange
+	id := "10"
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPut, Url+id, nil)
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusBadRequest, recorder.Code)
+}
+
+// Given PUT, when request is non-existing Player, then response status should be 404 (Not Found)
+func TestRequestPUTBodyNonExistingPlayerResponseStatusNotFound(test *testing.T) {
+
+	// Arrange
+	id := "999"
+	player := models.Player{
+		FirstName: "John",
+		LastName:  "Doe",
+	}
+	body, _ := json.Marshal(player)
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPut, Url+id, bytes.NewBuffer(body))
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusNotFound, recorder.Code)
+}
+
+// Given PUT, when request is existing Player, then response status should be 204 (No Content)
+func TestRequestPUTPBodyExistingPlayerResponseStatusNoContent(test *testing.T) {
+
+	// Arrange
+	id := "1"
+	player := GetExistingPlayer()
+	player.FirstName = "Emiliano"
+	player.MiddleName = ""
+	body, _ := json.Marshal(player)
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPut, Url+id, bytes.NewBuffer(body))
+	request.Header.Set(ContentType, ApplicationJSON)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusNoContent, recorder.Code)
+}
+
+// Given DELETE, when request is non-existing Player, then response status should be 404 (Not Found)
+func TestRequestDELETEIdNonExistingPlayerResponseStatusNotFound(test *testing.T) {
+
+	// Arrange
+	id := "999"
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodDelete, Url+id, nil)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusNotFound, recorder.Code)
+}
+
+// Given DELETE, when request is non-existing Player, then response status should be 404 (Not Found)
+func TestRequestDELETEIdExistingPlayerResponseStatusNoContent(test *testing.T) {
+
+	// Arrange
+	id := "12"
+	router := routes.Setup()
+	recorder := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodDelete, Url+id, nil)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	assert.Equal(test, http.StatusNoContent, recorder.Code)
 }
